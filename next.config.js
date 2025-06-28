3efe9f0f1998ed18f275244ef0c5ffd5c73cbd98
Travis Brown <Browntrasvis2001@gmail.com>
2025-06-27 20:55:12 -1000

    Optimize build configuration for faster deployments

--- next.config.js
+++ next.config.js
@@ -2,4 +2,19 @@ module.exports = {
   images: {
     domains: ['ui-avatars.com'],
   },
+  // Build optimizations
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
+  // Reduce bundle size
+  experimental: {
+    optimizeCss: true,
+  },
+  // Faster builds
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }; 
