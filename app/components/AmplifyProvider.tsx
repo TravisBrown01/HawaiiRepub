937b0018317a50739651364d3760c137a8e0514d
Travis Brown <Browntrasvis2001@gmail.com>
2025-06-27 20:20:15 -1000

    Include aws-exports.js in repository and fix import path

--- app/components/AmplifyProvider.tsx
+++ app/components/AmplifyProvider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
-import awsExports from '../aws-exports.js';
+import awsExports from '../aws-exports';
 
 export default function AmplifyProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
